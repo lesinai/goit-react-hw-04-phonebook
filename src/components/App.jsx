d75481d39535dd2920b452d4c45f665246066ef9
lesinai <90961075+lesinai@users.noreply.github.com>
2023-06-14 17:23:14 +0200

    upd 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { nanoid } from 'nanoid';
+
+export function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(
+    () => window.localStorage.setItem('contacts', JSON.stringify(contacts)),
+    [contacts]
+  );
+  const addContact = (name, number) => {
+    const added = contacts.some(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
+    );
+    if (added) {
+      Notify.failure(`${name} is already in contacts!`);
+    } else {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      setContacts(prevContacts => [newContact, ...prevContacts]);
+    }
+  };
+  const filterAll = e => {
+    setFilter(e.currentTarget.value);
+  };
+  const getContacts = () => {
+    const filtered = filter.toLowerCase();
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filtered));
+  };
+  const deleteContact = contactId =>
+    setContacts(contacts.filter(({ id }) => id !== contactId));
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterAll} />
+      <ContactList contacts={getContacts()} deleteContact={deleteContact} />
     </div>
   );
-};
+}
+
